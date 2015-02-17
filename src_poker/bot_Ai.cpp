ada0f70fd7de55203f2f66bb00235336b082770a
saxtheowl <saxtheowl@gmail.com>
2015-02-17 09:38:57 +0100

    ok

--- src_poker/bot_Ai.cpp
+++ src_poker/bot_Ai.cpp
@@ -38,7 +38,7 @@ void		bot_Ai::play_Bot(Board *board)
     }
 }
 */
-
+ /*
 void		bot_Ai::play_Bot(Board *board)
 {
   int		i;
@@ -70,6 +70,29 @@ void		bot_Ai::play_Bot(Board *board)
 	  f++;
 	}
       //      board->get_Competitors()[pos]->set_Standin(true);
+    }
+  else
+    {
+      board->get_Competitors()[pos]->set_Standin(false);
+      board->get_Competitors()[pos]->set_Played(true);
+    }
+}
+ */
+
+void		bot_Ai::play_Bot(Board *board)
+{
+  int		i;
+  int		f;
+  f = 0;
+  i = 0;
+  i = board->get_Current_Player();
+  std::cout << " first card = " << board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(0)]->get_True_Nb() << std::endl;
+  std::cout << " second card = " << board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(1)]->get_True_Nb() << std::endl;
+  if(board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(0)]->get_True_Nb() > 5 && board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(1)]->get_True_Nb() > 5)
+    {
+      board->get_Competitors()[pos]->set_Stack(board->get_Competitors()[pos]->get_Stack() - (board->get_Biggest_Raise() - board->get_Competitors()[pos]->get_Pushed()));
+      board->get_Competitors()[pos]->set_Pushed(board->get_Biggest_Raise());
+      board->get_Competitors()[pos]->set_Played(true);
     }
   else
     {
