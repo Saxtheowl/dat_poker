467c42fa08e488f44eee597772d4ca5f44646b9d
Saxtheowl <saxtheowl@gmail.com>
2016-04-17 00:30:17 +0200

    ok

--- src_poker/bot_Ai.cpp
+++ src_poker/bot_Ai.cpp
@@ -12,11 +12,13 @@ bot_Ai::~bot_Ai()
 void		bot_Ai::play_Bot(Board *board)
 {
   std::cout << "bot" << pos << " is playing " << std::endl;
+  if (board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(0)]->get_True_Nb() >
+      5 && board->get_Deck()[board->get_Competitors()[pos]->get_Index_Card(1)]->get_True_Nb() > 5)
     {
+      //  {
       if(board->get_Competitors()[pos]->get_Pushed() < board->get_Biggest_Raise())
 	{
 	  std::cout << "bot CALL get Pushed = " << board->get_Competitors()[pos]->get_Pushed() << " get biggest Raise = " << board->get_Biggest_Raise() << std::endl;
-	  std::cout << "bot CALL" << std::endl;
 	  board->get_Competitors()[pos]->set_Stack(board->get_Competitors()[pos]->get_Stack() - (board->get_Biggest_Raise() - board->get_Competitors()[pos]->get_Pushed()));
 	  board->get_Competitors()[pos]->set_Pushed(board->get_Biggest_Raise());
 	  std::cout << "bot CALL" << std::endl;
@@ -25,6 +27,12 @@ void		bot_Ai::play_Bot(Board *board)
 	{
 	  std::cout << "bot CHECK" << std::endl;
 	}
-      board->get_Competitors()[pos]->set_Played(true);
     }
+  else
+    {
+      board->get_Competitors()[pos]->set_Standin(false);
+      std::cout << "bot FOLD" << std::endl;
+    }
+  board->get_Competitors()[pos]->set_Played(true);
+  //  }
 }
